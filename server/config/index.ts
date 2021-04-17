3b314ee1f1a78e0431707abc9b726acdcb9fba9a
Ivan Statkevich <statkevich.ivan999@gmail.com>
2021-04-17 23:24:33 +0300

    fixed production url

--- server/config/index.ts
+++ server/config/index.ts
@@ -9,8 +9,8 @@ export default {
     GOOGLE_CLIENT_ID: '15920841265-21h9i95u2gqfp4h3lqs5o2spmijqrluh.apps.googleusercontent.com',
     GOOGLE_CLIENT_SECRET: '8rnedKZqxX_O3iigb9tqfGu2'
   },
-  SERVER_ROOT_URI: process.env.prod ? 'https://netxillmessenger.herokuapp.com/' : 'http://localhost:3000/',
-  CLIENT_ROOT_URI: process.env.prod ? 'https://netxillmessenger.herokuapp.com/' : 'http://localhost:8080/',
+  SERVER_ROOT_URI: process.env.production ? 'https://netxillmessenger.herokuapp.com/' : 'http://localhost:3000/',
+  CLIENT_ROOT_URI: process.env.production ? 'https://netxillmessenger.herokuapp.com/' : 'http://localhost:8080/',
   CLIENT_PATH: process.env.prod ? 'build/index.html' : 'client/index.html',
   JWT_SECRET: 'eyJhbGciOiJIUzI1NiJ9.eyJDcmVhdGVkQXQiOiIyMDIxLTA0LTE3VDE1OjAzOjIzLjA4NloiLCJEZWxldGVkQXQiOiIyMDIxLTA0LTE3VDE1OjAzOjIzLjA4NloifQ.AeAQBs-0uuKtBO8Q70-9EGj12TYga2l1C2cMnK49jlM',
 };
